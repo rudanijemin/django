c27c7e67651bf9d082669da7828d5c264c599738
jemin <rudanijemin18@gmail.com>
2021-04-07 19:44:50 +0530

    add html and change setting

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is homepage")
+    return render(request,"index.html")
 
 def about(request):
     return HttpResponse("this is about page")
--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'my_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
